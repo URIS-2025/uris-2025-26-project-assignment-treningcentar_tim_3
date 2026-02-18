780996994e9ac5801806719385f0e9da93d2a3eb
milicammm <milica.misan123@gmail.com>
2026-02-18 12:35:23 +0100

    Initial Payment entity, DTOs, enums, DbContext setup

---
+++ PaymentService/PaymentService/Context/PaymentContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using PaymentService.Models;
+
+namespace PaymentService.Context
+{
+    public class PaymentContext :DbContext
+    {
+        public PaymentContext(DbContextOptions<PaymentContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Payment> Payments { get; set; }
+    }
+}
