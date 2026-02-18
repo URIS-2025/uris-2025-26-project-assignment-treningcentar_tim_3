2dc7dc76ddce7fc04fc627b6f643d29375c74b75
maarijaaa <markovic.it3.2022@uns.ac.rs>
2026-02-18 14:01:54 +0100

    gitignore

--- AuthService/AuthService/obj/Debug/net8.0/AuthService.AssemblyInfo.cs
+++ AuthService/AuthService/obj/Debug/net8.0/AuthService.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AuthService")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f9b2afe8e4ecc4cc6b55f33c3aeed48a066e17e4")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2bb4f8fed85353048b621c3e0ed8841802e8f235")]
 [assembly: System.Reflection.AssemblyProductAttribute("AuthService")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AuthService")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
