322905a7096283722a96f75eae5cf3da39a33195
VanjaSkoric <vanjaskoric42@gmail.com>
2026-02-19 15:23:39 +0100

    Setup PostgreSQL database and EF Core initial migration

---
+++ ServiceService/ServiceService/Domain/Entities/Service.cs
@@ -0,0 +1,14 @@
+﻿using ServiceService.Domain.Enums;
+
+namespace ServiceService.Domain.Entities
+{
+    public class Service
+    {
+        public int ServiceId { get; set; } 
+        public string Name { get; set; } = string.Empty;
+        public string? Description { get; set; }
+        public decimal Price { get; set; }
+        public ServiceCategory Category { get; set; }
+
+    }
+}
