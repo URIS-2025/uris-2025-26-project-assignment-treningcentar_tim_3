d08e22de9a9c832eed651a8724f11daf02e94fa1
VanjaSkoric <vanjaskoric42@gmail.com>
2026-02-23 14:54:28 +0100

    Communication with Logger

---
+++ ServiceService/ServiceService/ServiceCalls/Logger/LoggerService.cs
@@ -0,0 +1,31 @@
+﻿using ServiceService.ServiceCalls.Logger.DTO;
+
+namespace ServiceService.ServiceCalls.Logger
+{
+    public class LoggerService : ILoggerService
+    {
+        private readonly HttpClient _http;
+
+        public LoggerService(HttpClient http)
+        {
+            _http = http;
+        }
+
+        public async Task TryLogAsync(LogCreationDTO dto, CancellationToken ct = default)
+        {
+            try
+            {
+               
+                dto.TimestampUtc ??= DateTime.UtcNow;
+
+                // Logger endpoint: POST api/logger
+                using var res = await _http.PostAsJsonAsync("api/logger", dto, ct);
+
+            }
+            catch
+            {
+                // swallow — logger je opcioni
+            }
+        }
+    }
+}
