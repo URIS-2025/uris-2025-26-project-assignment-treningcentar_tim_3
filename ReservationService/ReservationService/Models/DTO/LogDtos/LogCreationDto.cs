df8227b42f72536c2a51d53e68bf2cc45b42ff3c
markoisajlovic1 <markoisajlovic38@gmail.com>
2026-02-23 19:08:18 +0100

    reservation logger comunication

---
+++ ReservationService/ReservationService/Models/DTO/LogDtos/LogCreationDto.cs
@@ -0,0 +1,15 @@
+namespace ReservationService.Models.DTO.LogDtos;
+
+public class LogCreationDto
+{
+    public LogLevels Level { get; set; }
+    public string ServiceName { get; set; } = string.Empty;
+    public string Action { get; set; } = string.Empty;
+    public string Message { get; set; } = string.Empty;
+    public string? Details { get; set; }
+    public string? CorrelationId { get; set; }
+    public string? EntityType { get; set; }
+    public Guid? EntityId { get; set; }
+    public Guid? UserId { get; set; }
+    public DateTime? TimestampUtc { get; set; }
+}
